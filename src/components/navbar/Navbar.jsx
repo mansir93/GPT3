1db83359951c90ade7e0b78d01bcb08427de437d
Mansir <mansiraziz93@gmail.com>
2023-05-04 04:11:50 +0000

    initial

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+import "./navbar.css";
+// BEM --> Block Element Modifier
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#">Home</a>
+    </p>
+    <p>
+      <a href="#">WhatGPT3</a>
+    </p>
+    <p>
+      <a href="#">OpenAI</a>
+    </p>
+    <p>
+      <a href="#">Case Studies</a>
+    </p>
+    <p>
+      <a href="#">Library</a>
+    </p>
+  </>
+);
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  console.log(toggleMenu);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-signs">
+                <p>sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
